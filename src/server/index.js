896a378584b4222c90bad4c54e7e978afdf02be5
aleks-kudriavtsev <37224730+aleks-kudriavtsev@users.noreply.github.com>
2026-01-13 11:13:22 +0300

    Add run summary aggregation and UI

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ const protocols = require('./protocols');
 const validation = require('./validation');
 const stepLogger = require('./logging/stepLogger');
 const uploads = require('./uploads');
+const summary = require('./summary');
+const app = require('./app');
 
 module.exports = {
   models,
@@ -10,4 +12,6 @@ module.exports = {
   validation,
   stepLogger,
   uploads,
+  summary,
+  app,
 };
